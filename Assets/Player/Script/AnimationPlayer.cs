daca7b138ba922cb0557b4cb0d224c4831112cd6
Whaid Cremery <whaid.cremery@outlook.fr>
2020-02-14 11:25:54 +0100

    WCR - first commit

---
+++ Assets/Player/Script/AnimationPlayer.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationPlayer : MonoBehaviour {
+
+    public Transform shoulder;
+
+    private Animator animator;
+
+	// Use this for initialization
+	void Start ()
+    {
+        animator = GetComponent<Animator>();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        animator.SetBool("isWalking", false);
+
+        if (Input.GetButton("Vertical") || Input.GetButton("Horizontal"))
+        {
+            animator.SetBool("isWalking", true);
+        }
+
+        if (animator.GetBool("isWalking") && Input.GetButtonDown("Fire3"))
+        {
+            animator.SetBool("isWalking", false);
+            animator.SetBool("isRunning", true);
+        }
+
+        if(animator.GetBool("isRunning") && Input.GetButtonUp("Fire3"))
+        {
+            animator.SetBool("isRunning", false);
+        }
+    }
+}
---
+++ Assets/Player/Script/AnimationWeapon.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationWeapon : MonoBehaviour {
+
+    public AudioSource weaponReloading;
+
+    private Animator anim;
+
+	void Start ()
+    {
+        anim = GetComponent<Animator>();
+	}
+	
+	void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            weaponReloading.Play();
+            anim.SetTrigger("Reload");
+        }	
+	}
+}
